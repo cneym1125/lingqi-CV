37fe18763fad7586ea04c526c3e7eab400830b72
Li Zhe <3240215750@qq.com>
2026-05-22 17:19:46 +0800

    remove: 删除导航栏和顶部进度条

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
-import { Nav } from './components/Nav'
 import { Hero } from './components/Hero'
 import { ExperienceSection } from './components/ExperienceSection'
 import { ProjectsSection } from './components/ProjectsSection'
 import { ContactSection } from './components/ContactSection'
-import { ScrollProgress } from './components/ScrollProgress'
 import { ContactCardHost } from './components/ContactCard'
 
 export default function App() {
   return (
     <div id="top" className="relative min-h-screen bg-zinc-50">
-      <ScrollProgress />
-      <Nav />
       <main>
         <Hero />
         <ExperienceSection />
